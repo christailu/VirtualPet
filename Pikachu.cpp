832b15f260bc6c01e490352c796ad0e109d60785
Christelle Mbemba <chmbem@cs.csis.work>
2023-09-21 00:46:33 -0500

    Add Parentheses missin

--- Pikachu.cpp
+++ Pikachu.cpp
@@ -1,14 +1,15 @@
-using namespace std;
-#include "Pikachu.h"
 
-Pikachu::Pikachu(const string& PikachuName,const string& speciesName, int LevelPower)Pokemon(PikachuName, speciesName) {
+#include "Pikachu.h"
+//constructor
+Pikachu::Pikachu(const string& PikachuName,const string& speciesName, int LevelPower):Pokemon(PikachuName, speciesName) {
 	electricPower = LevelPower;
 	health = 100;
+}
 
-int getElectricPower() const{
+int Pikachu:: getElectricPower() const{
 	return electricPower;
 }
 
 void Pikachu::useElectricShock(){
 	cout << "Attack with Electric Shock"<<endl;
-
+}
