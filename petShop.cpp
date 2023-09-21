e4c433266204f2257b3a426c0d4eede2a465af3a
Christelle Mbemba <chmbem@cs.csis.work>
2023-09-21 00:20:22 -0500

    Add instance of new classes on the main function

--- petShop.cpp
+++ petShop.cpp
@@ -1,5 +1,8 @@
 #include "VirtualPet.h"
 #include "Pokemon.h"
+#include "Pikachu.h"
+#include "Gatomon.h"
+#include "Digimon.h"
 
 int main() {
 	VirtualPet myLittleBoringPet("Basic Pet");
@@ -21,6 +24,34 @@ int main() {
 	cout << "Hunger: " << pokemanz.getHunger() << endl;
 	cout << "Health: " << pokemanz.getHealth() << endl;
 	cout << "Species: " << pokemanz.getSpecies() << endl;
+
+	//**********************************
+	//create  a digimon
+	Digimon MyDigimon("Agumon","Fire");
+	MyDigimon.useSpecialAttack();
+
+	//create a Pikachu
+	Pikachu myPikachu("Pika", 80);
+	myPikachu.useElectricShock();
+
+	//create a GAtomon
+	Gatomon myGatomon("Gato", "Water", 90);
+	myGatomon.useCatClawAttack();
+
+	//Pet information
+	cout << endl <<endl;
+	cout << "My Digimon: "<< endl;
+	cout << "Name: " << MyDigimon.getName() << endl;
+	cout << "Element Type: "<< MyDigimon.getElement() << endl<< endl;
+
+	cout << "My Pikachu: " <<endl;
+	cout << "Name: " << myPikachu.getName() << endl;
+	cout << "Species: " << myPikachu.getSpecies() << endl << endl;
+
+	cout << "My Gatomon: " <<endl;
+	cout << "Name: " << myGatomon.getName() << endl;
+	cout << "Element: " << myGatomon.getElement() << endl;
+	cout << "Cat Claw Sharpness: " << myGatomon.getCatClawSharpness() << endl;
 
 	return 0;
 }
